7adccf00a925431ad44e6eeeaaf4975871fd2e3d
Majikujanisch <q1KGy6d8AaS0>
2020-10-06 16:34:49 +0200

    functions done JavaFx still troubling

--- FreedayCalc/src/Main.java
+++ FreedayCalc/src/Main.java
@@ -1,19 +1,36 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.*;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    static int monday = 0, thuesday = 0, wednesday = 0, thursday = 0, friday = 0;
     public static void main(String[] args) {
-        LocalDate today = LocalDate.now();
-        int year = today.getYear();
-        int monday = 0, thuesday = 0, wednesday = 0, thursday = 0, friday = 0;
-        int dayi = today.getDayOfMonth();
-        int monthi = today.getMonthValue();
-        int yeari = today.getYear();
-        //Definierung der Feiertage
+        //Variablen
+        int year = 2000, durationYears, fixedyear;
+
         ArrayList<LocalDate> freedays = new ArrayList<>();
+        Scanner User = new Scanner(System.in);
+
+        //eingabe durch den User
+        System.out.println("Startjahr angeben: ");
+        year = User.nextInt();
+        fixedyear = year;
+        System.out.println("Dauer in Jahren angeben: ");
+        durationYears = User.nextInt();
+
+        //Definierung der Feiertage
         do {
             try (Scanner scanner = new Scanner(new File("D:\\Schule\\Vierte+\\Programmieren\\Rubner\\Normal\\FreedayCalc\\FreeDays.csv"))) {
                 while (scanner.hasNextLine()) {
@@ -23,11 +40,11 @@ public class Main {
                 e.printStackTrace();
             }
             year++;
-        }while(year < today.getYear() + 10);
+        }while(year < fixedyear + durationYears);
+
         //überprüfung welche Tage frei sind
-        for(int  i = 0; i < freedays.size(); i++){
-            LocalDate date = freedays.get(i);
-            DayOfWeek dayS = date.getDayOfWeek();
+        for(LocalDate c:freedays){
+            DayOfWeek dayS = c.getDayOfWeek();
             switch (dayS) {
                 case MONDAY -> monday++;
                 case TUESDAY -> thuesday++;
@@ -37,28 +54,15 @@ public class Main {
 
             }
 
-
-            dayi++;
-            if(dayi > getDaysofMonth(monthi)){
-                dayi = 1;
-                monthi++;
-            }
-            if(monthi > 12) {
-                monthi = 1;
-                yeari++;
-            }
-
         }
-        System.out.print(monday);
-        System.out.println(" mondays");
-        System.out.print(thuesday);
-        System.out.println(" thuesdays");
-        System.out.print(wednesday);
-        System.out.println(" wednesdays");
-        System.out.print(thursday);
-        System.out.println(" thursdays");
-        System.out.print(friday);
-        System.out.println(" fridays");
+        monday += 2*durationYears;
+        thursday += 2*durationYears;
+
+
+        //Ausgabe der Daten
+        writeDays(monday,thuesday,wednesday,thursday,friday);
+        //JavaFX
+        launch(args);
 
 
     }
@@ -86,6 +90,58 @@ public class Main {
         };
         return days;
     }
+    public static void writeDays(int mon, int thue, int wed, int thur, int fri){
+        System.out.print(mon);
+        System.out.println(" mondays");
+        System.out.print(thue);
+        System.out.println(" thuesdays");
+        System.out.print(wed);
+        System.out.println(" wednesdays");
+        System.out.print(thur);
+        System.out.println(" thursdays");
+        System.out.print(fri);
+        System.out.println(" fridays");
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Angeben wie die Achsen sein sollen
+            NumberAxis xAxis = new NumberAxis();
+            CategoryAxis yAxis = new CategoryAxis();
 
+            // Anlegen der BarChart und angabe wie die Anordnung
+            BarChart<Number, String> barChart = new BarChart<Number, String>(xAxis, yAxis);
+            barChart.setTitle("Freie Tage im Vergleich");
+            xAxis.setLabel("Tage");
+            yAxis.setLabel("Wochentage");
+
+            // Anlegen einer Serie mit den jeweiligen Werten und dem Item
+            // dem sie zugeordnet werden.
+            XYChart.Series<Number, String> series1 = new XYChart.Series<Number, String>();
+            series1.setName("Days of week");
+            series1.getData().add(new XYChart.Data(monday, "mondays"));
+            series1.getData().add(new XYChart.Data(thuesday, "tuesdays"));
+            series1.getData().add(new XYChart.Data(wednesday, "wednesday"));
+            series1.getData().add(new XYChart.Data(thursday, "thursdays"));
+            series1.getData().add(new XYChart.Data(friday, "fridays"));
+
+
+            barChart.getData().add(series1);
+
+            VBox vbox = new VBox(barChart);
+
+            primaryStage.setTitle("Freie Tage im Vergleich");
+            Scene scene = new Scene(vbox, 400,200);
+
+            primaryStage.setScene(scene);
+            primaryStage.setHeight(300);
+            primaryStage.setWidth(400);
+
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+
+        }
+    }
 
 }
