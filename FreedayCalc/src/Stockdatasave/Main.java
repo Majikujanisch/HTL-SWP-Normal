f3065501e7288fa2e53c8ea4f8e99f72efa01532
Majikujanisch <karottennase123@gmail.com>
2020-12-02 22:36:55 +0100

    Datenbank wird eingerichtet und ausgegeben

--- FreedayCalc/src/Stockdatasave/Main.java
+++ FreedayCalc/src/Stockdatasave/Main.java
@@ -1,18 +1,28 @@
 package Stockdatasave;
 
+import com.mysql.cj.protocol.Resultset;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.*;
 import java.util.*;
 
 public class Main { // key IVB25ADTVUERPRXD
-    public static void main(String[] args) throws JSONException, MalformedURLException, IOException {
+    static Connection connection;
+    static String url = "jdbc:mysql://localhost/Aktienkurse?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    static String usernameDB="root";
+    static String passwordDB="f2idagAtc30U";
+    public static void main(String[] args) throws JSONException, MalformedURLException, IOException, ClassNotFoundException, SQLException {
         String URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=", key = "IVB25ADTVUERPRXD";
         Scanner user = new Scanner(System.in);
 
@@ -28,20 +38,85 @@ public class Main { // key IVB25ADTVUERPRXD
         LocalDate date = LocalDate.parse((json.getJSONObject("Meta Data").get("3. Last Refreshed")).toString());
         json = json.getJSONObject("Time Series (Daily)");
 
+        //MySQL
+        connectToMysql();
+        createTableMysql(ticker);
+
         for(int i = 0; i < 200; i++){
             try {
-                System.out.println(json.getJSONObject(date.toString()).get("4. close").toString());
+                createDataMysql(date,i, ticker, json.getJSONObject(date.toString()).get("4. close").toString());
             }
             catch(JSONException e){
-                System.out.println("Nicht vorhanden");
+                createDataMysql(date,i, ticker, "NULL");
             }
 
             date = date.minusDays(1);
         }
+        showMysql(ticker);
     }
 
 
     public static String buildURL(String url, String ticker, String key){
         return url = url + ticker.toUpperCase() + "&outputsize=full&apikey="+key;
+    }
+    public static boolean connectToMysql() throws ClassNotFoundException {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+
+            connection = DriverManager.getConnection(url, usernameDB, passwordDB);
+            return true;
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public static void createTableMysql(String ticker){
+        try{
+            connection = DriverManager.getConnection(url, usernameDB, passwordDB);
+            connection.createStatement().executeUpdate("create table if not exists "+ ticker +"(" +
+                    "Day varchar(10), inde int(4), close varchar(10), primary key(Day));");
+
+        }catch(Exception e){
+
+            System.out.println("false1");
+        }
+    }
+    public static void createDataMysql(LocalDate date, int index, String ticker,String close){
+
+        try{
+            connection = DriverManager.getConnection(url, usernameDB, passwordDB);
+            connection.createStatement().executeUpdate("insert into "+ ticker +" values('" + date + "','" + index  + "','" + close+ "')on Duplicate key update inde="+ index +";");
+
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("false2");
+        }
+    }
+    public static void showMysql(String ticker) throws SQLException {
+        ResultSet results = null;
+        try {
+            connection = DriverManager.getConnection(url, usernameDB, passwordDB);
+            try {
+                results = connection.createStatement().executeQuery("SELECT * from " + ticker + ";");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("false4");
+            }
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            System.out.println("false3");
+        }
+        System.out.printf("%10s   %10s   %10s %n", "Datum", "Index", "Close");
+        while (results.next()) {
+            System.out.printf("%10s   %10s   %10s%n",
+                    results.getString(1),
+                    results.getInt(2),
+                    results.getString(3)
+            );
+        }
     }
 }
