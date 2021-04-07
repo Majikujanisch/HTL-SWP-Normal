04093524ce6a5ca83b401bf9672ea75f724587d5
Majikujanisch <q1KGy6d8AaS0>
2021-04-07 15:29:02 +0200

    auslese der Ticker aus txt, insert nur wenn last date nicht heute, sleeptime in case dass kein Insert passiert und ein Ticker in weniger als 12 sec eingelesen ist(max von 5 anfragen pro minute)

--- FreedayCalc/src/StockdatasaveNEW/Main.java
+++
@@ -1,9 +0,0 @@
-package StockdatasaveNEW;
-
-public class Main {
-    /*
-        Databank db = new Databank(jdbc:mysql://localhost:3305/Aktienkurse?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC);
-    */
-
-}
-
